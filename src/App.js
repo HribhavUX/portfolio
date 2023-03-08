42615c3a8b4f56b91ba18ca48bbfcfeeda5f36b0
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-03-08 19:54:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
